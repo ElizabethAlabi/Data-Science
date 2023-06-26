8cb87a1e3e31045bbab97d4c2d2940d074a49653
Elizabeth Alabi <aadejoke0320@gmail.com>
2023-06-26 22:30:24 +0100

    helloworld version1

---
+++ hello_world.py
@@ -0,0 +1,10 @@
+# TASK 1
+#  python code to take in users name
+name= input("what is your name? ")
+print(name)
+# python code to take in users age
+age= int(input("how old are you? "))
+print(age)
+# print string Hello World!
+print("Hello World!")
+
